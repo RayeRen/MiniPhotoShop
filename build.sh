fdac8c9d81d0fd10bf16e64ef648f5fb7ccb5b6d
RayeRen <3150103692@zju.edu.cn>
2017-07-13 00:47:17 +0800

    update gitignore

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+#i686-w64-mingw32.static-qmake-qt5 && make \
 git checkout release && \
 git merge master && \
-#cmake . && make \
-#i686-w64-mingw32.static-qmake-qt5 && make \
 git add . && \
 git commit -m "release" && \
-git push -f origin release && \
-git checkout master 
+git push -f origin release 
