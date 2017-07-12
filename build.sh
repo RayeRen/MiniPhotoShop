9749f314012060dca46f60fdd6237051714c1583
RayeRen <3150103692@zju.edu.cn>
2017-07-13 00:35:35 +0800

    release

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#cmake . && make \
-#i686-w64-mingw32.static-qmake-qt5 && make \
 git checkout release && \
 git merge master && \
+#cmake . && make \
+#i686-w64-mingw32.static-qmake-qt5 && make \
 git add . && \
 git commit -m "release" && \
-git push -f origin release \
+git push -f origin release && \
+git checkout master 
