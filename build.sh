1507662561fc790281efc84f2f1c667afc887b3f
RayeRen <3150103692@zju.edu.cn>
2017-07-13 00:48:27 +0800

    delete ignored'

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#i686-w64-mingw32.static-qmake-qt5 && make \
-git checkout release && \
-git merge master && \
-git add . && \
-git commit -m "release" && \
-git push -f origin release 
